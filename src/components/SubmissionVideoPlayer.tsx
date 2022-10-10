df1845342d69d7bb91b34551cf9bfcdd6f8fdd15
oakleyaidan21 <36863200+oakleyaidan21@users.noreply.github.com>
2022-10-10 18:12:35 -0400

    scale videos

--- src/components/SubmissionVideoPlayer.tsx
+++ src/components/SubmissionVideoPlayer.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
+import { Dimensions } from 'react-native';
 import { Text, View } from 'react-native-ui-lib';
 import Video from 'react-native-video';
+
+const ww = Dimensions.get('window').width;
 
 interface SubmissionVideoPlayerProps {
   videoUrl: string;
@@ -12,15 +15,27 @@ const SubmissionVideoPlayer = ({
   shouldPlay,
 }: SubmissionVideoPlayerProps) => {
   const [paused, setPaused] = useState(!shouldPlay);
+  const [scaledHeight, setScaledHeight] = useState<number>();
 
   useEffect(() => setPaused(!shouldPlay), [shouldPlay]);
+
+  console.log('Scaled height', scaledHeight);
 
   return (
     <View>
       <Video
         source={{ uri: videoUrl }}
         paused={paused}
-        style={{ width: '100%', height: undefined, aspectRatio: 1 }}
+        resizeMode={'cover'}
+        style={{
+          width: ww,
+          height: scaledHeight,
+          aspectRatio: scaledHeight ? undefined : 1,
+        }}
+        onLoad={response => {
+          const { width, height } = response.naturalSize;
+          setScaledHeight(height * (ww / width));
+        }}
       />
     </View>
   );
