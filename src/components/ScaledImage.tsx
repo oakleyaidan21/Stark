ad9d8cb1d96a54d3f2c5fb0d1438a56035f539f6
oakleyaidan21 <36863200+oakleyaidan21@users.noreply.github.com>
2022-10-17 22:21:53 -0400

    revert ratio fetching for scaled image

--- src/components/ScaledImage.tsx
+++ src/components/ScaledImage.tsx
@@ -12,20 +12,21 @@ const ww = Dimensions.get('window').width;
 const ScaledImage = ({ url }: ScaledImageProps) => {
   const [height, setHeight] = useState<number>(0);
 
-  const onLoadEnd = (event: OnLoadEvent) => {
-    const {
-      nativeEvent: { width, height },
-    } = event;
-    const ratio = ww / width;
-    setHeight(height * ratio);
+  const getScaledHeight = () => {
+    Image.getSize(url, (w, h) => {
+      const ratio = ww / w;
+      setHeight(h * ratio);
+    });
   };
+
+  useEffect(getScaledHeight, []);
 
   const style = {
     width: '100%',
     height: height,
   };
 
-  return <FastImage source={{ uri: url }} style={style} onLoad={onLoadEnd} />;
+  return <FastImage source={{ uri: url }} style={style} />;
 };
 
 export default ScaledImage;
