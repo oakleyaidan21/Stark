a908159e2ee7011b8de04d6512814328ba5a7678
oakleyaidan21 <36863200+oakleyaidan21@users.noreply.github.com>
2023-02-19 14:50:43 -0500

    removed ignored configs cause this is a private project anyway

---
+++ src/config/snoowrapConfig.tsx
@@ -0,0 +1,8 @@
+const snoowrapConfig = {
+  userAgent: 'com.carrotcake.ARC:v0.1.0',
+  clientId: '0MPSAzNFJKhDHB_PszfUdw',
+  clientSecret: '',
+  refreshToken: '636069329713-HlybsfqH33XOWJb9Af4Pf1q6qLw',
+};
+
+export default snoowrapConfig;
