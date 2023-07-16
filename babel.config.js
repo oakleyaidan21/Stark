8b42feb38e9fcc83913af0318f8f4726a5b79cc3
Aidan Oakley <aoakley@Aidans-MacBook-Air.local>
2023-07-16 17:52:11 -0400

    other initial changes

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'], // reanimated needs to be listed last
+  plugins: [
+    '@babel/plugin-transform-private-methods',
+    ['react-native-reanimated/plugin', { loose: true }],
+  ], // reanimated needs to be listed last
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jestSetup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
 };
