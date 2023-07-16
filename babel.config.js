9f9da31f912793cf3bc8b32a105aee7ae745b9c3
Aidan Oakley <aoakley@Aidans-MacBook-Air.local>
2023-07-16 18:12:49 -0400

    runs on ios

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    '@babel/plugin-transform-private-methods',
-    ['react-native-reanimated/plugin', { loose: true }],
-  ], // reanimated needs to be listed last
+  plugins: ['react-native-reanimated/plugin'], // reanimated needs to be listed last
 };
--- metro.config.js
+++ metro.config.js
@@ -8,4 +8,4 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const config = {};
 
-module.exports = mergeConfig(getDefaultconfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
